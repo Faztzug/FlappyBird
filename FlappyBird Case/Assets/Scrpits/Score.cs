d203ff2c81fc00500f9501c4d30252a6bab46b53
Faztz Nya <faztzug@gmail.com>
2022-01-16 22:56:13 -0300

    Agora o score é escirto com imagens.

--- FlappyBird Case/Assets/Scrpits/Score.cs
+++ FlappyBird Case/Assets/Scrpits/Score.cs
@@ -2,24 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+//using TMPro;
 
 public class Score : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI scoreText;
+    //[SerializeField] private TextMeshProUGUI scoreText;
     //[HideInInspector] 
     public int currentScore;
-    
+    private char[] textScore;
+
+    private Image[] digits;
+
+    [SerializeField] GameObject numberPrefab;
+
+    [SerializeField] private Sprite n0;
+    [SerializeField] private Sprite n1;
+    [SerializeField] private Sprite n2;
+    [SerializeField] private Sprite n3;
+    [SerializeField] private Sprite n4;
+    [SerializeField] private Sprite n5;
+    [SerializeField] private Sprite n6;
+    [SerializeField] private Sprite n7;
+    [SerializeField] private Sprite n8;
+    [SerializeField] private Sprite n9;
+
 
 
     private void Start()
     {
         BirdController birdController = FindObjectOfType<BirdController>();
         birdController.onScorePoint += onUpdateScore;
-        
+
+        digits = GetComponentsInChildren<Image>();
 
 
-        scoreText = GetComponent<TextMeshProUGUI>();
+        //scoreText = GetComponentInChildren<TextMeshProUGUI>();
         currentScore = 0;
         onUpdateScore(0);
     }
@@ -27,11 +44,73 @@ public class Score : MonoBehaviour
     public void onUpdateScore(int value)
     {
         currentScore += value;
-        scoreText.text = currentScore.ToString();
+        textScore = currentScore.ToString().ToCharArray();
+
+
+        int textScoreLenght = textScore.Length;
+
+        if (textScoreLenght > digits.Length)
+        {
+            Instantiate(numberPrefab, this.gameObject.transform);
+
+            digits = GetComponentsInChildren<Image>();
+        }
+
+
+
+        for (int i = 0; i < textScoreLenght; i++)
+        {
+            switch (textScore[i])
+            {
+                case '0':
+                    digits[i].sprite = n0;
+                    break;
+                case '1':
+                    digits[i].sprite = n1;
+                    break;
+                case '2':
+                    digits[i].sprite = n2;
+                    break;
+                case '3':
+                    digits[i].sprite = n3;
+                    break;
+                case '4':
+                    digits[i].sprite = n4;
+                    break;
+                case '5':
+                    digits[i].sprite = n5;
+                    break;
+                case '6':
+                    digits[i].sprite = n6;
+                    break;
+                case '7':
+                    digits[i].sprite = n7;
+                    break;
+                case '8':
+                    digits[i].sprite = n8;
+                    break;
+                case '9':
+                    digits[i].sprite = n9;
+                    break;
+            }
+        }
+
+
+        foreach (char digit in textScore)
+        {
+            
+                
+        }
+        //scoreText.text = currentScore.ToString();
     }
 
     public void onGameOver()
     {
-        scoreText.text = "";
+        //scoreText.text = "";
+
+        foreach (Image digit in digits)
+        {
+            digit.gameObject.SetActive(false);
+        }
     }
 }
